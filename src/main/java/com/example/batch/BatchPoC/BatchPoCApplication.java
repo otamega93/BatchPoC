c17158b005ac5085b2fac240217693ca78598e8e
Manuel Páez <m.paez589@gmail.com>
2018-04-19 15:24:30 +0200

    First commit

---
+++ src/main/java/com/example/batch/BatchPoC/BatchPoCApplication.java
@@ -0,0 +1,12 @@
+package com.example.batch.BatchPoC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchPoCApplication.class, args);
+	}
+}
